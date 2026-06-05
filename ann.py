a46e9e62af1015b469285218915c67463db9550a
m-alladi2006 <alladimanjunadh07@gmail.com>
2026-06-05 20:10:17 +0530

    imported libraries and created training data

--- ann.py
+++ ann.py
@@ -1,1 +1,12 @@
-print("this is my first neural network")
+# Import Libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# create training data
+x=torch.tensor([[1000.0],[1200.0],[1500.0],1800.0],[2000.0]])
+y=torch.tensor([[20.0],25.0],[30.0],[35.0],[40.0]])
+
+# create neural network
+    
+
