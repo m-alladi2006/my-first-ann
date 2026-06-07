282b9d17c82e60c1627777440a3fc92026b82e28
m-alladi2006 <alladimanjunadh07@gmail.com>
2026-06-07 19:08:31 +0530

    created neural networks

--- ann.py
+++ ann.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 import torch.optim as optim
 
 # create training data
-x=torch.tensor([[1000.0],[1200.0],[1500.0],1800.0],[2000.0]])
-y=torch.tensor([[20.0],25.0],[30.0],[35.0],[40.0]])
+x=torch.tensor([[1000.0],[1200.0],[1500.0],[1800.0],[2000.0]])
+y=torch.tensor([[20.0],[25.0],[30.0],[35.0],[40.0]])
 
 # create neural network
 model=nn.sequential(
